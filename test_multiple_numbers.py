b26db31f6369dcca2ad7e01f2091dd158febafad
DiegoLopesAndrade <53227663+DiegoLopesAndrade@users.noreply.github.com>
2019-11-23 18:54:16 +0000

    Update test_multiple_numbers.py

--- test_multiple_numbers.py
+++ test_multiple_numbers.py
@@ -1,10 +1,16 @@
-import unittest
-import multiple_numbers
+import unittest # added library
+import multiple_numbers # referring another python core file
 
 
 class testMultiplesNumbers(unittest.TestCase):
+    """
+    Class to create functions to validate our numbers
+    """
 
     def test_multiples_numbers(self):
+        """
+        Function implementing a value to each case of number, multiple of 3, multiple of 5 or both
+        """
         multipleOfThree = multiple_numbers.multipleNumbers(18)
         multipleOfFive = multiple_numbers.multipleNumbers(35)
         multipleOfThreeAndFive = multiple_numbers.multipleNumbers(90)
